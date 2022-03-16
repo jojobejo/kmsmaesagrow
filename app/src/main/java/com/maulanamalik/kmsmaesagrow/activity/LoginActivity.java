6cc3afda70a77ba2ce96fbc107e282d23282b057
Maulana Malik Ibrahim <48055067+jojobejo@users.noreply.github.com>
2022-03-16 15:44:29 +0700

    Add files via upload
    new-project

---
+++ app/src/main/java/com/maulanamalik/kmsmaesagrow/activity/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.maulanamalik.kmsmaesagrow.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.maulanamalik.kmsmaesagrow.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
